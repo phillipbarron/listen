f6f283e1801080766545b62e6450b27ebff763cc
Phill Barron <phillipbarron242@gmail.com>
2018-12-25 11:45:56 +0000

    contrived commit

--- packages/server/language-service.js
+++ packages/server/language-service.js
@@ -1,9 +1,12 @@
 const googleLanguageClient = require('./google-translate-client');
 const awsLanguageClient = require('./aws-comprenend-client');
 
+const AWS = "AWS";
+const GOOGLE = "GOOGLE";
+
 const formatResponse = (response, service, text) => {
     switch(service) {
-        case "AWS":
+        case AWS:
             const result = response.ResultList;
             return result ? [{language: result[0].Languages[0].LanguageCode, confidence: result[0].Languages[0].Score, input: text }] : [];
         default:
@@ -13,9 +16,9 @@ const formatResponse = (response, service, text) => {
 
 const detectLanguage = async (service, text) => {
     switch(service) {
-        case "AWS":
+        case AWS:
             return formatResponse(await awsLanguageClient.detectLanguage(text), service, text);
-        case "GOOGLE":
+        case GOOGLE:
             return googleLanguageClient.detectLanguage(text);
         default:
             throw new Error(`unknown service ${service}`);
