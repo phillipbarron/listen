26dc4be88cffab5979133d94f041af33cef80f88
Phill Barron <phillipbarron242@gmail.com>
2018-12-27 10:14:14 +0000

    update todo

--- packages/server/google-translate-client.js
+++ packages/server/google-translate-client.js
@@ -3,7 +3,7 @@ const {
 } = require('@google-cloud/translate');
 
 // Creates a client
-// Move to a factory
+// todo - Move to a factory
 const translate = new Translate();
 
 const detectLanguage = async (text) => {
